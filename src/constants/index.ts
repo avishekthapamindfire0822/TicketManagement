9b9ea0de2b14a1d9627fbd17e2e5f9f6115b90eb
shrmavijay <vijay.sharma@mindfiresolutions.com>
2024-04-23 00:09:52 +0530

    made changes in schema according to the UI

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,11 @@
 export interface EnvironmentInterface {
   SERVER_PORT: number;
+  IP_ADDRESS : string;
 }
 
 const environment: EnvironmentInterface = {
   SERVER_PORT: parseInt(process.env.SERVER_PORT || "3000", 10),
+  IP_ADDRESS: process.env.IP_ADDRESS || 'localhost'
 };
 
 export default environment;
--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,17 @@ import RootRouter from "./routes";
 import { app } from "./app";
 import globalErrorHandler from "./utils/GlobalErrorHandler";
 import * as cors from "cors";
+// import {Request,Response} from 'express'
 const PORT = environment.SERVER_PORT || 3000;
+const ipAddress =environment.IP_ADDRESS
 
 app.use(cors())
 app.use(bodyParser.json());
 app.use("/api", RootRouter);
 app.use(globalErrorHandler)
-app.listen(PORT, () => {
+// app.get('/',(req:Request,res:Response)=>{
+//   res.send("Hello")
+// })
+app.listen(PORT,ipAddress, () => {
   console.log(`Server is running on port ${PORT}`);
 });
