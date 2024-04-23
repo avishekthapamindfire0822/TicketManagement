1eb5a7c959738e7c310915940f0aa6073dd96640
shrmavijay <vijay.sharma@mindfiresolutions.com>
2024-04-23 21:14:06 +0530

    added default route

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,11 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use("/api", RootRouter);
 app.use(globalErrorHandler)
-// app.get('/',(req:Request,res:Response)=>{
-//   res.send("Hello")
-// })
+
+app.get('/',(req,res)=>{
+  res.send('hello')
+})
+
 app.listen(PORT,ipAddress, () => {
   console.log(`Server is running on port ${PORT}`);
 });
